91a1db84731176f9ed630b3e3171ee7b719f393d
Leo <a131234568@gmail.com>
2022-04-14 12:02:18 +0800

    新增首頁

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-app.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 app.get('/users/login', (req, res) => {
