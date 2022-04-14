2f38f3754ef601d857d12b4b962fe3368be33359
Leo <a131234568@gmail.com>
2022-04-14 14:03:57 +0800

    重構2 完成

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
 const session = require('express-session')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const passport = require('passport')
-const bcrypt = require('bcryptjs')
+const flash = require('connect-flash')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const routes = require('./routes')
 const usePassport = require('./config/passport')
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
-app.use(express.urlencoded({ extended: true }))
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 
 app.use(routes)
 
